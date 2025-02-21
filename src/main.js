b6130ff810214fecee7d88ad6922d11852df177d
nimamesbah <nimamesbah85@gmail.com>
2025-02-21 14:59:19 +0330

    css better

--- src/main.js
+++ src/main.js
@@ -144,7 +144,7 @@ async function renderCart() {
         cartData.push(result)
     }
     if(cartData.length===0)
-        root.innerHTML=`<div class="flex flex-col gap-5 items-center w-full">
+        root.innerHTML=`<div class="flex flex-col gap-5 mb-44 items-center sm:mt-0 mt-52 w-full">
         <div class="w-max  mt-16 ">سبد شما خالیست</div>
         <div onclick="emptyCart()" class="w-max py-3.5 px-4 bg-blue-400 rounded-4xl hover:bg-gray-600 duration-200 cursor-pointer">بازگشت</div>
         
@@ -155,16 +155,16 @@ async function renderCart() {
     const template = cartData.map((item) => {
 
         return `
-        <div class="border flex flex-col gap-3  ">
-            <h3>${item.title}</h3>
+        <div class="border flex flex-col gap-3 items-center ">
+            <h3 class="max-h-7 overflow-hidden">${item.title}</h3>
             <img class="object-contain w-full h-96" src='${item.image}' width="200" />
-            <h4>amount:${CART.find(cartItem=> cartItem.id===item.id).quantity} </h4>
+            <h4>تعداد خرید: ${CART.find(cartItem=> cartItem.id===item.id).quantity} </h4>
             <div onclick='removeFromCart(${item.id})' class="w-max cursor-pointer rounded-lg bg-red-500 py-2 px-3" > حذف از سبد خرید</div>
 
         </div>
         `
     }).join("");
-    const container = `<div class="grid grid-cols-1 sm:grid-cols-4 mx-auto w-full gap-2.5 mt-[110px] sm:mt-0 ">${template}
+    const container = `<div class="grid grid-cols-1 sm:grid-cols-4 mx-auto w-full gap-6 mt-[110px] sm:mt-0 py-5 px-4 ">${template}
 
      </div>`
 
@@ -226,6 +226,17 @@ function renderProductCard({ id, price, image, title }) {
 }
 
 async function renderMainPage() {
+    root.innerHTML=`
+
+<div role="status" class="mx-auto mt-36 w-32">
+    <svg aria-hidden="true" class="w-32  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+
+`
     const products = await getLimitedProducts(4)
 
     const template = products.map(product => {
@@ -315,7 +326,7 @@ async function renderSingleProduct(){
             
         
     </a>
-    <div class="sm:w-1/3 w-4/5 h-max  sm:h-[400px] flex flex-col justify-between sm:items-start items-center gap-5 sm:gap-0  ">
+    <div class="sm:w-1/3 w-4/5 h-max  sm:h-[400px] flex mb-14 flex-col justify-between sm:items-start items-center gap-5 sm:gap-0  ">
         <h4>${description}</h4>
         <div class="w-max flex gap-3">
             <input class="inline w-14 border rounded-xl " id="quantityInput" type="number" min="1"/>
